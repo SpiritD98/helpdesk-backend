2ef1a1cf745f31c3e123bcb30ee6ad1830227c1f
JDiazD <u23232498@utp.edu.pe>
2026-04-15 21:04:43 -0500

    Se creo entidad: Rol

--- src/main/java/com/helpdesk/helpdesk_backend/model/Rol.java
+++ src/main/java/com/helpdesk/helpdesk_backend/model/Rol.java
@@ -1,5 +1,19 @@
 package com.helpdesk.helpdesk_backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table (name = "roles")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Rol {
-    
+  
+    @Id 
+    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
 }
