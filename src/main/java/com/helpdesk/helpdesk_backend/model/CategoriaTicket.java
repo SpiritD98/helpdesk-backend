e8fbb6b08f90d3d1f2da5117900c1c4bbfbf9af8
JDiazD <u23232498@utp.edu.pe>
2026-04-15 22:47:15 -0500

    Se completo la entidad CategoriaTicket

--- src/main/java/com/helpdesk/helpdesk_backend/model/CategoriaTicket.java
+++ src/main/java/com/helpdesk/helpdesk_backend/model/CategoriaTicket.java
@@ -1,5 +1,27 @@
 package com.helpdesk.helpdesk_backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categorias_ticket")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class CategoriaTicket {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String descripcion ;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "empresa_id", nullable = false)
+    private Empresa empresa;
 }
