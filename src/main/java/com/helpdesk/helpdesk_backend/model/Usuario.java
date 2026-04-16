21ed3694a2d5154ad2f96bfdef08b52a65894503
JDiazD <u23232498@utp.edu.pe>
2026-04-15 21:08:45 -0500

    Se agrego en model/Usuario:  @JsonIgnore en password, fechaCreacion con updatable = false

--- src/main/java/com/helpdesk/helpdesk_backend/model/Usuario.java
+++ src/main/java/com/helpdesk/helpdesk_backend/model/Usuario.java
@@ -1,5 +1,9 @@
 package com.helpdesk.helpdesk_backend.model;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,30 +21,35 @@ import lombok.Setter;
 @Builder
 
 public class Usuario {
-     @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
- 
+
     @Column(nullable = false, length = 100)
     private String nombres;
- 
+
     @Column(nullable = false, length = 100)
     private String apellidos;
- 
+
     @Column(nullable = false, unique = true, length = 150)
     private String email;
- 
+
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
- 
-    @Column(nullable = false)
+
     @Builder.Default
+    @Column(nullable = false)
     private Boolean estado = true;
- 
+
+    @Builder.Default
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "empresa_id", nullable = false)
     private Empresa empresa;
- 
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "rol_id", nullable = false)
     private Rol rol;
