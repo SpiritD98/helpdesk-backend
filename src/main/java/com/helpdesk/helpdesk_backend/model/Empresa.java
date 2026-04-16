64bfa5dde84654ccba4455ff087d98000717365c
JDiazD <u23232498@utp.edu.pe>
2026-04-15 20:25:27 -0500

    Se creo entidad: Empresa

--- src/main/java/com/helpdesk/helpdesk_backend/model/Empresa.java
+++ src/main/java/com/helpdesk/helpdesk_backend/model/Empresa.java
@@ -1,5 +1,36 @@
 package com.helpdesk.helpdesk_backend.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "empresas")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String ruc;
+
+    @Column(nullable = false)
+    private String correoContacto;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean estado = true;
+
+    @Builder.Default
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
     
 }
